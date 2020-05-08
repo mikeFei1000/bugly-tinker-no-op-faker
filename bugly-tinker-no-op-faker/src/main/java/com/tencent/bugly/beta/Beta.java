b4ac2268b094034ffb6930a690eb1d8007728d13
mikefei <fei.zhao@longbridge.sg>
2020-05-08 15:00:48 +0800

    初始化代码

---
+++ bugly-tinker-no-op-faker/src/main/java/com/tencent/bugly/beta/Beta.java
@@ -0,0 +1,17 @@
+package com.tencent.bugly.beta;
+
+import android.content.Context;
+
+/**
+ * @author zhaofei
+ */
+public class Beta {
+
+    public static void installTinker(Object applicationLike) {
+
+    }
+
+    public static void setIsDevelopmentDevice(Context context, boolean isDevelopmentDevice) {
+
+    }
+}
